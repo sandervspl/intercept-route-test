a3f976eaf6cbcedb03b20a458a8b4a9458cb1c40
Sander Vispoel <sandervispoel@hotmail.com>
2023-04-07 14:33:48 +0200

    Move main styles to layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout(props: any) {
   console.log(props.modal);
 
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center p-24">
       {props.modal}
       {props.children}
     </main>
