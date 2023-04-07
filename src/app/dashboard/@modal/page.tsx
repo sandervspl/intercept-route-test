325038dbd9827c87261bf59488b80a770cd65d1f
Sander Vispoel <sandervispoel@hotmail.com>
2023-04-07 15:42:45 +0200

    Delete dashboard

--- src/app/dashboard/@modal/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function InterceptedUser() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="absolute bg-red-500/10 w-1/2 h-1/2">
-        I am an intercepted user page
-      </div>
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link
-        href="/dashboard"
+        href="/en/foo/bar/baz"
         className="rounded-xl border-2 border-gray-400 p-2"
       >
-        To dashboard
+        To foo/bar/baz
       </Link>
     </main>
   );
