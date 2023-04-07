c4e55b3ce3f116455db30214e4cede07f1851edc
Sander Vispoel <sandervispoel@hotmail.com>
2023-04-07 21:42:50 +0200

    Use proper route names?

--- src/app/[locale]/user/[user]/page.tsx
+++ src/app/[locale]/user/[user]/page.tsx
@@ -1,7 +1,7 @@
 export default function User() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      I am a user page
+      I am a REGULAR user page
     </main>
   );
 }
